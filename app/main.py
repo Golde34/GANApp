af8f20ae7daed894adb8f9f7946345bf02cc15b8
Golde34 <nguyendongducviet2001@gmail.com>
2023-07-22 11:00:44 +0700

    fix bug + rqmt

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ async def register(request: Request,
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    db_user = crud.get_user_by_username(db, email=user.username)
+    db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
         raise HTTPException(status_code=400, detail="Username already registered")
 
